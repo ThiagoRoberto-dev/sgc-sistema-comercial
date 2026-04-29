b2b42db471516b3132661d14489a176eeb8c087a
“ThiagoRoberto-dev” <gestaothg2.0@gmail.com>
2026-04-29 12:41:39 -0300

    Projeto inicial SGC

---
+++ src/main/java/com/thiago/sgc_sistema/SgcSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.sgc_sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcSistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/sgc_sistema/SgcSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.sgc_sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
